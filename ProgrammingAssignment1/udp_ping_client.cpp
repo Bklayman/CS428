3de61b9c4700f9fca4b1d3830fa378303fd143fd
Brendan Klayman <bklayma1@binghamton.edu>
2021-10-07 17:34:36 -0400

    Added comments to programming assignment 1

--- ProgrammingAssignment1/udp_ping_client.cpp
+++ ProgrammingAssignment1/udp_ping_client.cpp
@@ -24,6 +24,7 @@ int main() {
 	struct sockaddr_in servaddr;
 	struct timeval timeout;
 	fd_set readfds;
+	int msgCount = 10;
 
 	// Create a UDP socket
 	// Notice the use of SOCK_DGRAM for UDP packets
@@ -38,17 +39,17 @@ int main() {
 	servaddr.sin_family = AF_INET; // IPv4
 	servaddr.sin_addr.s_addr = INADDR_ANY; // localhost
 	servaddr.sin_port = htons(PORT); // port number
+	len = sizeof(servaddr); // Address length
 
-	// Set the timeout variable to one second
+	// Set the timeout variables for use in select()
 	timeout.tv_sec = 1;
 	timeout.tv_usec = 0;
-
 	FD_ZERO(&readfds);
 	FD_SET(sockfd, &readfds);
 
-	len = sizeof(servaddr);
+  for(int i = 0; i < msgCount; i++) {
 
-  for(int i = 0; i < 10; i++) {
+		// Obtain current time and send a ping to the server
 	  chrono::high_resolution_clock::time_point before = chrono::high_resolution_clock::now();
 		if((sendto(sockfd, (const char *)buffer, strlen(buffer),
 			MSG_CONFIRM, (const struct sockaddr *) &servaddr, len)) < 0){
@@ -56,24 +57,27 @@ int main() {
 			exit(1);
 		}
 
+		// Equivalent to recvfrom, except it will detect timout after timeout.tv_sec seconds, which is set to 1
 		n = select(32, &readfds, NULL, NULL, &timeout);
 		if(n <= 0){
 
 			cout << "Timeout: No message received within 1 second" << endl;
+
+			// Reset the variables associated with detecting a timeout within select()
 			timeout.tv_sec = 1;
 			timeout.tv_usec = 0;
-
 			FD_ZERO(&readfds);
 			FD_SET(sockfd, &readfds);
 
 		} else {
-		
+
+			// No timeout, receive message, get current time, and print difference between both obtained times
 			n = recvfrom(sockfd, (char *)buffer, sizeof(buffer),
 				MSG_WAITALL, ( struct sockaddr *) &servaddr, &len);
 			buffer[n] = '\0';
 			chrono::high_resolution_clock::time_point after = chrono::high_resolution_clock::now();
 			chrono::duration<double, milli> duration = after - before;
-			cout << "RTT: " << duration.count() << endl;
+			cout << "RTT: " << duration.count() << "ms" << endl;
 		}
 	}
 	return 0;
