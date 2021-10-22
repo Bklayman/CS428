fd0b172b332576a45b87ec32a35586032616ec48
Brendan Klayman <bklayma1@cs.binghamton.edu>
2021-10-22 18:07:28 -0400

    Project 2: Sends message with wrong length. Progress :)

--- ProgrammingAssignment2/Client1.cpp
+++ ProgrammingAssignment2/Client1.cpp
@@ -11,24 +11,21 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <chrono>
 
-#define PORT	 12008
+#define PORT	 12002
 
 using namespace std;
 
 int main() {
 	int sockfd, n;
-	socklen_t len;
 	char buffer[1024];
 	struct sockaddr_in servaddr;
-	struct timeval timeout;
-	fd_set readfds;
 	int msgCount = 10;
+	socklen_t len;
 
 	// Create a UDP socket
 	// Notice the use of SOCK_DGRAM for UDP packets
-	if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
 		cout << "Socket Creation Error" << endl;
 		exit(1);
 	}
@@ -41,44 +38,35 @@ int main() {
 	servaddr.sin_port = htons(PORT); // port number
 	len = sizeof(servaddr); // Address length
 
-	// Set the timeout variables for use in select()
-	timeout.tv_sec = 1;
-	timeout.tv_usec = 0;
-	FD_ZERO(&readfds);
-	FD_SET(sockfd, &readfds);
+	if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) != 0){
+		cout << "Connect Error" << endl;
+		exit(1);
+	}
 
-  for(int i = 0; i < msgCount; i++) {
-
+  	for(int i = 0; i < msgCount; i++) {
 		// Obtain current time and send a ping to the server
-	  chrono::high_resolution_clock::time_point before = chrono::high_resolution_clock::now();
-		if((sendto(sockfd, (const char *)buffer, strlen(buffer),
+		/*if((sendto(sockfd, (const char *)buffer, strlen(buffer),
 			MSG_CONFIRM, (const struct sockaddr *) &servaddr, len)) < 0){
 			cout << "Send Error" << endl;
 			exit(1);
 		}
 
-		// Equivalent to recvfrom, except it will detect timout after timeout.tv_sec seconds, which is set to 1
-		n = select(32, &readfds, NULL, NULL, &timeout);
-		if(n <= 0){
+		// No timeout, receive message, get current time, and print difference between both obtained times
+		n = recvfrom(sockfd, (char *)buffer, sizeof(buffer),
+			MSG_WAITALL, ( struct sockaddr *) &servaddr, &len);
+		buffer[n] = '\0';
+		cout << "Received" << endl;*/
 
-			cout << "Timeout: No message received within 1 second" << endl;
+		//write(sockfd, (char*)buffer, sizeof(buffer));
+		sendto(sockfd, "Hi", strlen("Hi"), MSG_CONFIRM, (const struct sockaddr*) &servaddr, len);
 
-			// Reset the variables associated with detecting a timeout within select()
-			timeout.tv_sec = 1;
-			timeout.tv_usec = 0;
-			FD_ZERO(&readfds);
-			FD_SET(sockfd, &readfds);
+		//memset(buffer, 0, sizeof(buffer));
 
-		} else {
+		//n = read(sockfd, (char*)buffer, sizeof(buffer));
 
-			// No timeout, receive message, get current time, and print difference between both obtained times
-			n = recvfrom(sockfd, (char *)buffer, sizeof(buffer),
-				MSG_WAITALL, ( struct sockaddr *) &servaddr, &len);
-			buffer[n] = '\0';
-			chrono::high_resolution_clock::time_point after = chrono::high_resolution_clock::now();
-			chrono::duration<double, milli> duration = after - before;
-			cout << "RTT: " << duration.count() << "ms" << endl;
-		}
+		//if(n > 0){
+		//	cout << buffer << endl;
+		//}
 	}
 	return 0;
 }
