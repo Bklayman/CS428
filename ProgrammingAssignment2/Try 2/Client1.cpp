ca27b9754f23d61e24ec96d1bd10783e6c7db8d3
Brendan Klayman <bklayma1@binghamton.edu>
2021-10-25 19:54:28 -0400

    Added assigned function to project 2

--- ProgrammingAssignment2/Try 2/Client1.cpp
+++ ProgrammingAssignment2/Try 2/Client1.cpp
@@ -7,40 +7,21 @@
 #define MAX 80
 #define PORT 8081
 #define SA struct sockaddr
-void func(int sockfd)
-{
-    char buff[MAX];
-    int n;
-    for (;;) {
-        bzero(buff, sizeof(buff));
-        printf("Client1: Enter the string : ");
-        n = 0;
-        while ((buff[n++] = getchar()) != '\n')
-            ;
-        write(sockfd, buff, sizeof(buff));
-        bzero(buff, sizeof(buff));
-        read(sockfd, buff, sizeof(buff));
-        printf("Client1: From Server : %s", buff);
-        if ((strncmp(buff, "exit", 4)) == 0) {
-            printf("Client1: Client Exit...\n");
-            break;
-        }
-    }
-}
 
 int main()
 {
-    int sockfd, connfd;
+    int sockfd, connfd, n;
     struct sockaddr_in servaddr, cli;
+		char buff[MAX];
+		char sent1[MAX];
 
     // socket create and varification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
-        printf("Client1: socket creation failed...\n");
-        exit(0);
+        printf("Client1: Socket Creation Error\n");
+        exit(1);
     }
-    else
-        printf("Client1: Socket successfully created..\n");
+
     bzero(&servaddr, sizeof(servaddr));
 
     // assign IP, PORT
@@ -50,14 +31,18 @@ int main()
 
     // connect the client socket to server socket
     if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
-        printf("Client1: connection with the server failed...\n");
-        exit(0);
+        printf("Client1: Connect Error\n");
+        exit(1);
     }
-    else
-        printf("Client1: connected to the server..\n");
 
-    // function for chat
-    func(sockfd);
+    bzero(buff, sizeof(buff));
+		buff = "Client X: Alice\n";
+		sent1 = buff;
+    write(sockfd, buff, sizeof(buff));
+    bzero(buff, sizeof(buff));
+    read(sockfd, buff, sizeof(buff));
+    printf("%s", sent1);
+		prints("%s", buff);
 
     // close the socket
     close(sockfd);
